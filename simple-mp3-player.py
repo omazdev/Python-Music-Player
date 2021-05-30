79c83e8de77049a9d608891f90767f8129d3eb5b
omazdev <omazdev@gmail.com>
2021-05-30 01:19:24 +0100

    Added track play time and track length.

--- simple-mp3-player.py
+++ simple-mp3-player.py
@@ -3,32 +3,54 @@ from tkinter import ttk, filedialog
 import tkinter
 import pygame
 import os
+import time
+from mutagen import File
 
 
 def play_pause(event=None):
-
+    """ Play or Pause curent track and update track status label accordingly """
     if playlist_box.get(ACTIVE):  # Check if playlist is not empty
 
+        global previous_active_track_idx
+        
         if not playlist_box.curselection():  # If no track is selected, select the first track
             playlist_box.selection_set(0)
+            # Keep record of selected track index (this variable is created only once at startup)
+            selected_track_idx = 0
 
         track_name = playlist_box.get(playlist_box.curselection())
 
         if track_title.get() != track_name or track_status.get() == "(Stopped)" or event:  # event is a doube clic on a track
-            track_idx = int(playlist_box.curselection()[0])
-            track_path = playlist[track_idx]
-            # Display Selected track title
-            track_title.set(track_name)
-            # Displa track Status
-            track_status.set("(Playing)")
+            selected_track_idx = int(playlist_box.curselection()[0])
+            track_path = playlist[selected_track_idx]
 
-            play_pause_btn.configure(image=pause_img)
+            # pygame.mixer.music.load(track_path)
+            # pygame.mixer.music.play()
+
+            # track_length(track_path)
+            # play_time()
+
             try:
                 pygame.mixer.music.load(track_path)
                 pygame.mixer.music.play()
+                # Display Selected track title
+                track_title.set(track_name)
+                # Displa track Status
+                track_status.set("(Playing)")
+                play_pause_btn.configure(image=pause_img)
+
+                track_length(track_path)
+                play_time()
+                previous_active_track_idx = int(selected_track_idx)
             except:
                 tkinter.messagebox.showwarning(
-                    title="Warning!", message="Unable to play current track. Please choose another track!")
+                    title="Warning!", message=f"Audio file incorrect : {track_name}, Please chose another file!")
+                # Uselect the incorrect track
+                playlist_box.selection_clear(selected_track_idx)
+                # Select the previous active track
+                playlist_box.selection_set(previous_active_track_idx)
+                playlist_box.see(previous_active_track_idx)
+
 
         elif track_status.get() == "(Playing)":
             # Displaying Status
@@ -50,18 +72,20 @@ def play_pause(event=None):
 
 
 def stopsong():
+    """" Stop playing current track """
     # Displaying Status
     track_status.set("(Stopped)")
     play_pause_btn.configure(image=play_img)
+    track_pos_label.configure(text="00:00")
     # Stopped Song
     pygame.mixer.music.stop()
 
 
 def insert_tracks(event=None):
-
+    """" Open dialog to insert audio files """
     # playlist_items = playlist_box.get(0, len(playlist))
     # Fetching tracks
-    tracks_items= filedialog.askopenfilenames(filetypes=[("Audio files", ('*.flac', '*.wav', '*.mp3'))],
+    tracks_items= filedialog.askopenfilenames(filetypes=[("Audio files", ('*.flac', '*.wav', '*.mp3', '.ogg'))],
                                               title="Select tracks")
 
     # Force insertion of at least one track
@@ -79,7 +103,39 @@ def insert_tracks(event=None):
 # TODO Implement remove tracks
 
 def change_vol(vol_event=None):
+    """" Control track volume """
     pygame.mixer.music.set_volume(float(vol_event))
+
+def play_time():
+    """" Print track time position """
+    # Print track position only if the track is playing
+    if not track_status.get() == "(Stopped)":
+        # Get current track time in seconds
+        current_time = pygame.mixer.music.get_pos()/1000 
+        # Convert to format minutes:seconds
+        current_time_formated = time.strftime('%M:%S', time.gmtime(current_time))
+
+        # Print play time in the label 
+        track_pos_label.configure(text=current_time_formated)
+
+        # Update label every 1 second
+        track_pos_label.after(1000, play_time)
+
+
+def track_length(track_path):
+    """ Extract and print track length """
+    track_extension = os.path.splitext(track_path)[1]
+    if track_extension:
+        try:
+            mutagen_track = File(track_path) 
+            track_length = mutagen_track.info.length
+        except:
+            track_length = 0
+            tkinter.messagebox.showwarning(
+                title="Warning!", message=f"Audio file incorrect : {track_path}")
+        finally:
+            track_length_formated = time.strftime('%M:%S', time.gmtime(track_length))
+            track_length_label.configure(text=track_length_formated)
 
 
 def remove_tracks():
@@ -115,6 +171,7 @@ track_title.set("---  : ")
 track_status = StringVar()
 track_status.set("---")
 
+
 # Configure Menus
 menubar = Menu(root)
 root.configure(menu=menubar)
@@ -131,13 +188,13 @@ menu_file.add_command(label='Remove tracks')
 menu_file.add_command(label='Quit', command= quit_app, accelerator="Ctrl+Q")
 root.bind('<Control-q>', quit_app)
 
-main_frame = ttk.Frame(root, padding="10 10 10 10")
+main_frame = ttk.Frame(root, padding="5 5 5 5")
 main_frame.grid(column=0, row=0)
 main_frame.columnconfigure(0, weight=1)
 main_frame.rowconfigure(0, weight=1)
 
 # Creating Playlist Frame
-songs_frame = ttk.Frame(main_frame, padding="10 10 10 10")
+songs_frame = ttk.Frame(main_frame, padding="5 5 5 5")
 songs_frame.grid(column=0, row=0)
 
 # Inserting scrollbar
@@ -146,28 +203,48 @@ scrol_y = Scrollbar(songs_frame, orient=VERTICAL)
 # Inserting Playlist listbox
 playlist_box = Listbox(songs_frame, width=60, height=10, yscrollcommand=scrol_y.set, selectbackground="grey",
                        selectmode=SINGLE, font=("times new roman", 12, "bold"), bg="white", fg="navyblue")
-# Handle double click on playlist item
-playlist_box.bind('<Double-Button-1>', play_pause)
+
 # Applying Scrollbar to listbox
 scrol_y.pack(side=RIGHT, fill=Y)
 scrol_y.config(command=playlist_box.yview)
 playlist_box.pack(fill=BOTH)
 
+# Handle double click on playlist item
+playlist_box.bind('<Double-Button-1>', play_pause)
+
+previous_active_track_idx = 0
+
 # Track Frame for Song infos
-track_frame = ttk.Frame(main_frame, padding="10 10 10 10")
+track_frame = ttk.Frame(main_frame, padding="1 1 1 1")
 track_frame.grid(column=0, row=1)
 
 track_title_label = Label(track_frame, text="Song Title", textvariable=track_title, font=(
     "times new roman", 12, "bold"), fg="navyblue")
-track_title_label.pack(side=LEFT)
+track_title_label.grid(row=0, column=0)
 
 track_status_label = Label(track_frame, text="Song Title", textvariable=track_status, font=(
     "times new roman", 12, "bold"), fg="navyblue")
-track_status_label.pack()
+track_status_label.grid(row=0, column=1)
+
+# Track Frame for Song slider and play time
+track_pos_frame = ttk.Frame(main_frame, padding="1 1 1 1")
+track_pos_frame.grid(column=0, row=2)
+
+# track_pos = StringVar(value="0:0")
+track_pos_label = Label(track_pos_frame, text="00:00")
+track_pos_label.grid(row=1, column=0)
+
+track_length_label = Label(track_pos_frame, text="00:00")
+track_length_label.grid(row=1, column=2)
+
+track_pos_slider = Scale(track_pos_frame, length=400, from_=1, to=0,
+                         orient=HORIZONTAL, resolution=.1, showvalue=False, command=change_vol)
+track_pos_slider.grid(row=1, column=1)
+
 
 # Buttons Frame
-buttons_frame = ttk.Frame(main_frame, padding="10 10 10 10")
-buttons_frame.grid(column=0, row=2)
+buttons_frame = ttk.Frame(main_frame, padding="2 2 2 2")
+buttons_frame.grid(column=0, row=3)
 
 # Load Play Image
 play_img = PhotoImage(
@@ -193,14 +270,7 @@ stop_btn = Button(buttons_frame, image=stop_img,
 stop_btn.grid(row=0, column=1)
 
 # Volume Slider
-volume_ctrl = Scale(
-    buttons_frame,
-    from_ = 1,
-    to = 0,
-    orient = VERTICAL,
-    resolution = .1,
-    command=change_vol
-)
+volume_ctrl = Scale(buttons_frame, from_ = 1, to = 0, orient = VERTICAL, resolution = .1, command=change_vol)
 volume_ctrl.grid(row=0, column=2)
 # Set volume's initial value
 volume_ctrl.set(0.6)
