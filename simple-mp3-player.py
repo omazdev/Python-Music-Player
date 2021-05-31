1b39792cb365b352f31cff1661f4e5fd0ef8de17
omazdev <omazdev@gmail.com>
2021-05-31 16:13:28 +0100

    Added seek in track slider.

--- simple-mp3-player.py
+++ simple-mp3-player.py
@@ -30,26 +30,37 @@ def play_pause(event=None):
             # track_length(track_path)
             # play_time()
 
-            try:
-                pygame.mixer.music.load(track_path)
-                pygame.mixer.music.play()
-                # Display Selected track title
-                track_title.set(track_name)
-                # Displa track Status
-                track_status.set("(Playing)")
-                play_pause_btn.configure(image=pause_img)
+            pygame.mixer.music.load(track_path)
+            pygame.mixer.music.play()
+            # Display Selected track title
+            track_title.set(track_name)
+            # Displa track Status
+            track_status.set("(Playing)")
+            play_pause_btn.configure(image=pause_img)
 
-                track_length(track_path)
-                play_time()
-                previous_active_track_idx = int(selected_track_idx)
-            except:
-                tkinter.messagebox.showwarning(
-                    title="Warning!", message=f"Audio file incorrect : {track_name}, Please chose another file!")
-                # Uselect the incorrect track
-                playlist_box.selection_clear(selected_track_idx)
-                # Select the previous active track
-                playlist_box.selection_set(previous_active_track_idx)
-                playlist_box.see(previous_active_track_idx)
+            track_length(track_path)
+            play_time()
+            previous_active_track_idx = int(selected_track_idx)
+            # try:
+            #     pygame.mixer.music.load(track_path)
+            #     pygame.mixer.music.play()
+            #     # Display Selected track title
+            #     track_title.set(track_name)
+            #     # Displa track Status
+            #     track_status.set("(Playing)")
+            #     play_pause_btn.configure(image=pause_img)
+
+            #     track_length(track_path)
+            #     play_time()
+            #     previous_active_track_idx = int(selected_track_idx)
+            # except:
+            #     tkinter.messagebox.showwarning(
+            #         title="Warning!", message=f"Audio file incorrect : {track_name}, Please chose another file!")
+            #     # Uselect the incorrect track
+            #     playlist_box.selection_clear(selected_track_idx)
+            #     # Select the previous active track
+            #     playlist_box.selection_set(previous_active_track_idx)
+            #     playlist_box.see(previous_active_track_idx)
 
 
         elif track_status.get() == "(Playing)":
@@ -106,36 +117,73 @@ def change_vol(vol_event=None):
     """" Control track volume """
     pygame.mixer.music.set_volume(float(vol_event))
 
-def play_time():
+def play_time(value=None):
     """" Print track time position """
+    global update_pos
+    global track_total_play_time
+    global pos_list
     # Print track position only if the track is playing
     if not track_status.get() == "(Stopped)":
+        
         # Get current track time in seconds
-        current_time = pygame.mixer.music.get_pos()/1000 
-        # Convert to format minutes:seconds
-        current_time_formated = time.strftime('%M:%S', time.gmtime(current_time))
+        track_total_play_time = pygame.mixer.music.get_pos()/1000
 
-        # Print play time in the label 
-        track_pos_label.configure(text=current_time_formated)
+        if not value and pygame.mixer.music.get_busy():
+            if pos_list[-1] == 'SLIDED':
+                track_total_play_time += float(pos_list[-2])
+                # pos_list.pop()
+            # Convert to format minutes:seconds
+            current_time_formated = time.strftime('%M:%S', time.gmtime(track_total_play_time)) 
+            # current_time += 1
+            print("current time: ", track_total_play_time, "slider position: ", track_pos.get())
+
+            # update slider position
+            track_pos.set(track_total_play_time)
+            # Print play time in the label 
+            track_pos_label.configure(text=current_time_formated)
+
+        else:
+            print("LAST SLIDER POS: ", pos_list[-1])
+            pygame.mixer.music.play(0, float(pos_list[-1]) )
+            track_total_play_time += float(pos_list[-1])
+            pos_list.append(pos_list[-1])
+            pos_list.append('SLIDED')
+
+            # Convert to format minutes:seconds
+            current_time_formated = time.strftime('%M:%S', time.gmtime(float(pos_list[-2])))
+            track_pos_label.configure(text=current_time_formated)
+            print("after current time: ", pos_list[-2], "slider position: ", track_pos.get())
 
         # Update label every 1 second
-        track_pos_label.after(1000, play_time)
+        update_pos = track_pos_label.after(1000, play_time)
 
 
 def track_length(track_path):
     """ Extract and print track length """
+    global track_total_length
     track_extension = os.path.splitext(track_path)[1]
     if track_extension:
         try:
             mutagen_track = File(track_path) 
-            track_length = mutagen_track.info.length
+            track_total_length = mutagen_track.info.length
         except:
-            track_length = 0
+            track_total_length = 0
             tkinter.messagebox.showwarning(
                 title="Warning!", message=f"Audio file incorrect : {track_path}")
         finally:
-            track_length_formated = time.strftime('%M:%S', time.gmtime(track_length))
+            track_length_formated = time.strftime('%M:%S', time.gmtime(track_total_length))
             track_length_label.configure(text=track_length_formated)
+            track_pos_slider.configure(to=track_total_length)
+
+ 
+def slider_watcher(value):
+    global update_pos
+    track_pos_label.after_cancel(update_pos)
+    print("VALUE", value)
+    pos_list.append(value)
+    pygame.mixer.music.stop()
+    # track_pos_label.after(100,play_time(pos_list[-1]))
+    play_time(value)
 
 
 def remove_tracks():
@@ -159,6 +207,7 @@ root.option_add('*tearOff', FALSE)
 # Initiating Pygame
 pygame.init()
 # Initiating Pygame Mixer
+pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.mixer.init()
 # pygame.mixer.music.set_volume(0.2)
 
@@ -171,7 +220,13 @@ track_title.set("---  : ")
 track_status = StringVar()
 track_status.set("---")
 
+track_pos = DoubleVar()
+track_pos.set(0)
+update_pos = None
 
+pos_list = [0]
+
+track_total_play_time = 0
 # Configure Menus
 menubar = Menu(root)
 root.configure(menu=menubar)
@@ -237,8 +292,9 @@ track_pos_label.grid(row=1, column=0)
 track_length_label = Label(track_pos_frame, text="00:00")
 track_length_label.grid(row=1, column=2)
 
-track_pos_slider = Scale(track_pos_frame, length=400, from_=1, to=0,
-                         orient=HORIZONTAL, resolution=.1, showvalue=False, command=change_vol)
+
+track_pos_slider = Scale(track_pos_frame, length=400, from_=1, to=0, orient=HORIZONTAL,
+                         resolution=.001, showvalue=False, variable=track_pos, command=slider_watcher)
 track_pos_slider.grid(row=1, column=1)
 
 
